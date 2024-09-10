17832335aa67d7e6a66493673986db9828a348ac
Vitalya Yakovlev <vitaalya717@mail.ru>
2024-09-10 16:49:34 +0300

    new code #2

--- src/main/java/ru/netology/statistic/Task.java
+++ src/main/java/ru/netology/statistic/Task.java
@@ -12,7 +12,13 @@ public class Task {
     public int getId() {
         return id;
     }
-
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Task task = (Task) o;
+        return id == task.id;
+    }
     @Override
     public int hashCode() {
         return Objects.hash(id);
